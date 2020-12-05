520fe8ac233c6aed00a0ffcf6d802893eeefd8e2
S0nic014 <47799401+S0nic014@users.noreply.github.com>
2020-12-05 02:35:28 +0000

    fix missing res folder in build script

--- build_windows.sh
+++ build_windows.sh
@@ -3,13 +3,14 @@ APP_NAME="file_sorter"
 MAIN_FILE_PATH=file_sorter/main.py
 SITE_PACKAGES_PATH=.env/Lib/site-packages
 CONFIG_FILE="config.ini;."
-RES_FOLDER="res/;."
+RES_FOLDER="res/;res/"
 ICON_PATH=res/images/icon.ico
 
 py -3 -m PyInstaller --onedir --noconsole \
 --icon $ICON_PATH \
 --paths $SITE_PACKAGES_PATH \
 --add-data=$CONFIG_FILE \
+--add-data=$RES_FOLDER \
 -n $APP_NAME \
 $MAIN_FILE_PATH
 
